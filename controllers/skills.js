2969f976ff87a06718f6c8c2e7e80d62999c75f7
Dawson Myers <Djmyers2002@gmail.com>
2023-04-13 16:55:30 -0400

    Initial Commit

---
+++ controllers/skills.js
@@ -0,0 +1,63 @@
+import { Skill } from '../models/skill.js'
+
+function index(req, res) {
+  Skill.find({})
+  .then (skills => {
+    res.render('skills/index', {
+      skills: skills,
+    })
+  })
+    .catch(error => {
+      console.log(error)
+      res.redirect('/')
+    })
+  }
+  
+function newSkill(req, res) {
+    res.render('skills/new')
+  }
+  
+function create(req, res) {
+console.log(req.body)
+    Skill.create(req.body)
+    .then( skill => {
+      console.log(skill)
+      res.redirect('/skills')
+    })
+    .catch(error => {
+      console.log(error)
+      res.redirect('/skills')
+    })
+  }
+
+function show(req, res) {
+  Skill.findById(req.params.skillId)
+  .then(skill => {
+    res.render('skills/show', {
+      skill: skill
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/skills')
+  })
+}
+
+function deleteSkill(req, res) {
+  Skill.findByIdAndDelete(req.params.todoId)
+  .then(skill => {
+    res.redirect('/skills')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/skills')
+  })
+}
+
+  export {
+    index,
+    show,
+    newSkill as new,
+    create,
+    deleteSkill as delete,
+  }
---
+++ data/skill-data.js
@@ -0,0 +1,11 @@
+
+
+const skills = [
+    {text: 'Can Read', done: true, _id: 125223},
+    {text: 'Can Write', done: true, _id: 127904},
+    {text: 'Can Breathe Air', done: true,  _id: 139608},
+  ]
+
+export {
+    skills
+}
